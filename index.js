5d1faead868141975af9c3c4b083bdf6dfd2621c
Christopher Frieler <christopher@frieler.rocks>
2021-04-07 10:05:49 +0200

    upload artifacts on failed gradle-executions as well.
    Otherwise you would miss results of failed tests for example.

--- index.js
+++ index.js
@@ -23,21 +23,23 @@ async function run() {
 
         await exec.exec('chmod +x gradlew');
 
-        const gradleArguments = core.getInput('arguments', { required: true }).replace(/\n/g, " ");
-        await exec.exec(`./gradlew --no-daemon --stacktrace ${gradleArguments}`);
-
-        const artifacts = core.getInput("artifacts")
-        if (artifacts) {
-            const artifactClient = artifact.create()
-            const artifactUploadOptions = {
-                continueOnError: false
-            }
-            for (const artifact of artifacts.split("\n")) {
-                const artifactDeclaration = artifact.split(" ", 2);
-                const artifactName = artifactDeclaration[0];
-                const artifactPathGlobber = await glob.create(artifactDeclaration[1]);
-                const artifactPath = await artifactPathGlobber.glob()
-                await artifactClient.uploadArtifact(artifactName, artifactPath, '.', artifactUploadOptions)
+        try {
+            const gradleArguments = core.getInput('arguments', { required: true }).replace(/\n/g, " ");
+            await exec.exec(`./gradlew --no-daemon --stacktrace ${gradleArguments}`);
+        } finally {
+            const artifacts = core.getInput("artifacts")
+            if (artifacts) {
+                const artifactClient = artifact.create()
+                const artifactUploadOptions = {
+                    continueOnError: false
+                }
+                for (const artifact of artifacts.split("\n")) {
+                    const artifactDeclaration = artifact.split(" ", 2);
+                    const artifactName = artifactDeclaration[0];
+                    const artifactPathGlobber = await glob.create(artifactDeclaration[1]);
+                    const artifactPath = await artifactPathGlobber.glob()
+                    await artifactClient.uploadArtifact(artifactName, artifactPath, '.', artifactUploadOptions)
+                }
             }
         }
 
